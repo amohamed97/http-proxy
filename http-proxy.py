6fde3d4aca8f8a33b157173cabfb210a62f8df34
Ahmed Dewidar <a.mo.dewidar@gmail.com>
2020-04-08 15:04:48 +0200

    removed irrelevant docstrings

--- http-proxy.py
+++ http-proxy.py
@@ -1,4 +1,3 @@
-# Don't forget to change this file's name before submission.
 import sys
 import os
 import enum
@@ -11,15 +10,6 @@ class HttpRequestInfo(object):
     """
     Represents a HTTP request information
 
-    Since you'll need to standardize all requests you get
-    as specified by the document, after you parse the
-    request from the TCP packet put the information you
-    get in this object.
-
-    To send the request to the remote server, call to_http_string
-    on this object, convert that string to bytes then send it in
-    the socket.
-
     client_address_info: address of the client;
     the client of the proxy, which sent the HTTP request.
 
@@ -30,8 +20,6 @@ class HttpRequestInfo(object):
 
     requested_path: path of the requested resource, without
     including the website name.
-
-    NOTE: you need to implement to_http_string() for this class.
     """
 
     def __init__(self, client_info, method: str, requested_host: str,
@@ -43,34 +31,16 @@ class HttpRequestInfo(object):
         self.requested_host = requested_host
         self.requested_port = requested_port
         self.requested_path = requested_path
-        # Headers will be represented as a list of tuples
-        # for example ("Host", "www.google.com")
-        # if you get a header as:
-        # "Host: www.google.com:80"
-        # convert it to ("Host", "www.google.com") note that the
-        # port is removed (because it goes into the request_port variable)
         self.headers = headers
 
     def to_http_string(self):
         """
-        Convert the HTTP request/response
+        Converts the HTTP request/response
         to a valid HTTP string.
-        As the protocol specifies:
 
-        [request_line]\r\n
-        [header]\r\n
-        [headers..]\r\n
-        \r\n
-
-        You still need to convert this string
-        to byte array before sending it to the socket,
-        keeping it as a string in this stage is to ease
-        debugging and testing.
         """
         return "\r\n".join([f'{self.method} {self.requested_path} HTTP/1.0', "\r\n".join([f'{name}: {value}' for name, value in self.headers]),"\r\n"])
-        # print("*" * 50)
-        # print("[to_http_string] Implement me!")
-        # print("*" * 50)
+
 
     def to_byte_array(self, http_string):
         """
@@ -100,7 +70,6 @@ class HttpErrorResponse(object):
         self.message = message
 
     def to_http_string(self):
-        """ Same as above """
         return f"{self.code} {self.message}"
 
     def to_byte_array(self, http_string):
@@ -114,12 +83,6 @@ class HttpErrorResponse(object):
 
 
 class HttpRequestState(enum.Enum):
-    """
-    The values here have nothing to do with
-    response values i.e. 400, 502, ..etc.
-
-    Leave this as is, feel free to add yours.
-    """
     INVALID_INPUT = 0
     NOT_SUPPORTED = 1
     GOOD = 2
@@ -127,42 +90,17 @@ class HttpRequestState(enum.Enum):
 
 
 def entry_point(proxy_port_number):
-    """
-    Entry point, start your code here.
-
-    Please don't delete this function,
-    but feel free to modify the code
-    inside it.
-    """
-
     server = setup_sockets(proxy_port_number)
     serve_clients(server)
-    # print("*" * 50)
-    # print("[entry_point] Implement me!")
-    # print("*" * 50)
     return None
 
 
 def setup_sockets(proxy_port_number):
-    """
-    Socket logic MUST NOT be written in the any
-    class. Classes know nothing about the sockets.
-
-    But feel free to add your own classes/functions.
-
-    Feel free to delete this function.
-    """
     print("Starting HTTP proxy on port:", proxy_port_number)
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.setblocking(0)
     s.bind((socket.gethostname(), proxy_port_number))
-    # when calling socket.listen() pass a number
-    # that's larger than 10 to avoid rejecting
-    # connections automatically.
-    # print("*" * 50)
-    # print("[setup_sockets] Implement me!")
-    # print("*" * 50)
     return s
 
 def serve_clients(server):
@@ -263,17 +201,9 @@ def http_request_pipeline(source_addr, http_raw_data):
     returns:
      HttpRequestInfo if the request was parsed correctly.
      HttpErrorResponse if the request was invalid.
-
-    Please don't remove this function, but feel
-    free to change its content
     """
     # Parse HTTP request
     parsed = parse_http_request(source_addr, http_raw_data)
-
-    # Validate, sanitize, return Http object.
-    # print("*" * 50)
-    # print("[http_request_pipeline] Implement me!")
-    # print("*" * 50)
     return parsed
 
 
@@ -300,10 +230,6 @@ def parse_http_request(source_addr, http_raw_data) -> HttpRequestInfo:
         host_address = ""
         host_port = -1
     request_line_parts = request_line.split()
-    # print("*" * 50)
-    # print("[parse_http_request] Implement me!")
-    # print("*" * 50)
-    # Replace this line with the correct values.
     ret = HttpRequestInfo(
         source_addr, request_line_parts[0], host_address, host_port, request_line_parts[1], headers)
     return sanitize_http_request(ret)
@@ -317,10 +243,6 @@ def check_http_request_validity(http_request_info: HttpRequestInfo) -> HttpReque
     One of values in HttpRequestState
     """
     valid_verbs = ("GET", "HEAD", "POST", "PUT", "DELETE", "CONNECT", "OPTIONS", "TRACE", "PATCH")
-    # print("*" * 50)
-    # print("[check_http_request_validity] Implement me!")
-    # print("*" * 50)
-    # return HttpRequestState.GOOD (for example)
     if http_request_info.requested_host == "":
         return HttpRequestState.INVALID_INPUT
     if http_request_info.method not in valid_verbs:
@@ -335,12 +257,6 @@ def check_http_request_validity(http_request_info: HttpRequestInfo) -> HttpReque
 def sanitize_http_request(request_info: HttpRequestInfo) -> HttpRequestInfo:
     """
     Puts an HTTP request on the sanitized (standard form)
-
-    returns:
-    A modified object of the HttpRequestInfo with
-    sanitized fields
-
-    for example, expand a URL to relative path + Host header.
     """
     if request_info.requested_host == "":
         request_info.requested_path = request_info.requested_path.replace(
@@ -353,21 +269,12 @@ def sanitize_http_request(request_info: HttpRequestInfo) -> HttpRequestInfo:
             request_info.requested_host = hostname_parts[0]
             request_info.requested_path = "/" + "/".join(path_parts[1:])
             request_info.headers.append(("Host", request_info.requested_host))
-    # print("*" * 50)
-    # print("[sanitize_http_request] Implement me!")
-    # print("*" * 50)
     return request_info
-    # ret = HttpRequestInfo(None, None, None, None, None, None)
-    # return ret
-
-#######################################
-# Leave the code below as is.
-#######################################
 
 
 def get_arg(param_index, default=None):
     """
-        Gets a command line argument by index (note: index starts from 1)
+        Gets a command line argument by index
         If the argument is not supplies, it tries to use a default value.
 
         If a default value isn't supplied, an error message is printed
@@ -382,35 +289,13 @@ def get_arg(param_index, default=None):
             print(e)
             print(
                 f"[FATAL] The comand-line argument #[{param_index}] is missing")
-            exit(-1)    # Program execution failed.
-
-
-def check_file_name():
-    """
-    Checks if this file has a valid name for *submission*
-
-    leave this function and as and don't use it. it's just
-    to notify you if you're submitting a file with a correct
-    name.
-    """
-    script_name = os.path.basename(__file__)
-    import re
-    matches = re.findall(r"(\d{4}_){2}lab2\.py", script_name)
-    if not matches:
-        print(f"[WARN] File name is invalid [{script_name}]")
+            exit(-1)
 
 
 def main():
-    """
-    Please leave the code in this function as is.
-
-    To add code that uses sockets, feel free to add functions
-    above main and outside the classes.
-    """
     print("\n\n")
     print("*" * 50)
     print(f"[LOG] Printing command line arguments [{', '.join(sys.argv)}]")
-    check_file_name()
     print("*" * 50)
 
     # This argument is optional, defaults to 18888
